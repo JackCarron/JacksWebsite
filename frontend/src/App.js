a86fee034b9f3bd75ff73674b671b553e70482cc
Jack Carron <jackcarron@Jacks-MacBook-Pro-6.local>
2019-12-17 20:25:37 -0700

    Some styling fixes

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Projects from "./components/projects.component.js";
+import About from "./components/about.component.js";
+import Contact from "./components/contact.component.js";
+import Header from "./components/header.component.js"
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header/>
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Projects} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/components/header.component.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import logo from "../Logo.png";
+
+class Header extends Component {
+  render() {
+    return (
+<nav className="navbar navbar-expand-lg text-light bg-dark mr-auto h5">
+            <a class="navbar-brand" href="http://www.jcarron.net">
+              <img src={logo} width="75" height="75" alt="Jack Carron" />
+            </a>
+            <Link to="/" className="navbar-brand">Jack Carron</Link>
+            <div className="collpase navbar-collapse hover ">
+              <ul className="navbar-nav">
+                <li className="navbar-item">
+                  <Link to="/about" className="nav-link">About</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/projects" className="nav-link">Projects</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to='/contact' className="nav-link">Contact Me</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+      );
+    }
+  }
+  
+  export default Header;
