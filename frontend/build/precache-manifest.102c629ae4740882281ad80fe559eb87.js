f5aadd32555cb93e0d11149c0ff6ef49b7cde712
Ubuntu <ubuntu@ip-172-31-39-112.us-east-2.compute.internal>
2020-04-11 16:34:48 +0000

    4-11 prod

---
+++ frontend/build/precache-manifest.102c629ae4740882281ad80fe559eb87.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6ae7a1dbfdfd48c520172b2ea054a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f306fa29d082583730",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6208382c6125f32082ad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "78f306fa29d082583730",
+    "url": "/static/js/2.7048d1d4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7048d1d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6208382c6125f32082ad",
+    "url": "/static/js/main.a53097da.chunk.js"
+  },
+  {
+    "revision": "e16d2bfa5db88669cd65",
+    "url": "/static/js/runtime-main.29ec529b.js"
+  },
+  {
+    "revision": "381ced9f22684d2978bcf2608c559ab4",
+    "url": "/static/media/Golf.381ced9f.PNG"
+  },
+  {
+    "revision": "f5491286bb8c7f04a0e7cfd1a8dd165d",
+    "url": "/static/media/Jack Skate N.Quezada-8955.f5491286.JPG"
+  },
+  {
+    "revision": "7b9db18a81794b140151a4ae30ef239e",
+    "url": "/static/media/Logo.7b9db18a.png"
+  },
+  {
+    "revision": "781fed2aade2db8435c086d9a2e0db37",
+    "url": "/static/media/Professional.781fed2a.JPG"
+  },
+  {
+    "revision": "8ff53910a82f22fbaf0be4fd56cdf583",
+    "url": "/static/media/rory-the-tiger-1.8ff53910.png"
+  }
+]);
